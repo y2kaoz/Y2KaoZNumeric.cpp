cfed49b8427c44c6e8c30f2f249326df2730bd60
Carlos Gonzalez Y2KaoZ <y2kaoz@gmail.com>
2022-02-09 12:51:46 -0400

    minor tweaks

--- include/Y2KaoZ/Numeric/BigInt/BigInt.hpp
+++ include/Y2KaoZ/Numeric/BigInt/BigInt.hpp
@@ -8,27 +8,26 @@
 namespace Y2KaoZ::Numeric::BigInt {
 
 using BigInt = boost::multiprecision::cpp_int;
-using LittleUint8 = boost::endian::little_uint8_buf_t;
-using BigUint8 = boost::endian::big_uint8_buf_t;
 
 static_assert(sizeof(std::uint8_t) == sizeof(std::byte));
-
 static_assert(CHAR_BIT == 8); // NOLINT
 
 [[nodiscard]] Y2KAOZNUMERIC_EXPORT auto fromBuffer(std::span<const std::uint8_t> buffer, bool bigEndian = false)
   -> BigInt;
 [[nodiscard]] Y2KAOZNUMERIC_EXPORT auto fromBuffer(std::span<const std::byte> buffer, bool bigEndian = false) -> BigInt;
-[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto fromBuffer(std::span<const LittleUint8> buffer) -> BigInt;
-[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto fromBuffer(std::span<const BigUint8> buffer) -> BigInt;
+[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto fromBuffer(std::span<const boost::endian::little_uint8_buf_t> buffer) -> BigInt;
+[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto fromBuffer(std::span<const boost::endian::big_uint8_buf_t> buffer) -> BigInt;
 
 [[nodiscard]] Y2KAOZNUMERIC_EXPORT auto uintBuffer(const BigInt& bigInt, bool bigEndian = false)
   -> std::vector<std::uint8_t>;
 [[nodiscard]] Y2KAOZNUMERIC_EXPORT auto byteBuffer(const BigInt& bigInt, bool bigEndian = false)
   -> std::vector<std::byte>;
-[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto littleEndianBuffer(const BigInt& bigInt) -> std::vector<LittleUint8>;
-[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto bigEndianBuffer(const BigInt& bigInt) -> std::vector<BigUint8>;
+[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto littleEndianBuffer(const BigInt& bigInt)
+  -> std::vector<boost::endian::little_uint8_buf_t>;
+[[nodiscard]] Y2KAOZNUMERIC_EXPORT auto bigEndianBuffer(const BigInt& bigInt)
+  -> std::vector<boost::endian::big_uint8_buf_t>;
 
-template <std::size_t SIZE>
+Y2KAOZNUMERIC_EXPORT template <std::size_t SIZE>
 inline auto uintBuffer(const BigInt& bigInt, bool bigEndian = false) -> std::array<std::uint8_t, SIZE> {
   std::vector<std::uint8_t> vector = uintBuffer(bigInt, bigEndian);
   if (SIZE < vector.size()) {
@@ -40,7 +39,7 @@ inline auto uintBuffer(const BigInt& bigInt, bool bigEndian = false) -> std::arr
   return array;
 }
 
-template <std::size_t SIZE>
+Y2KAOZNUMERIC_EXPORT template <std::size_t SIZE>
 inline auto byteBuffer(const BigInt& bigInt, bool bigEndian = false) -> std::array<std::byte, SIZE> {
   std::vector<std::byte> vector = byteBuffer(bigInt, bigEndian);
   if (SIZE < vector.size()) {
@@ -52,13 +51,13 @@ inline auto byteBuffer(const BigInt& bigInt, bool bigEndian = false) -> std::arr
   return array;
 }
 
-template <std::size_t SIZE>
-auto littleEndianBuffer(const BigInt& bigInt) -> std::array<boost::endian::little_uint8_buf_t, SIZE> {
+Y2KAOZNUMERIC_EXPORT template <std::size_t SIZE>
+inline auto littleEndianBuffer(const BigInt& bigInt) -> std::array<boost::endian::little_uint8_buf_t, SIZE> {
   return std::bit_cast<std::array<boost::endian::little_uint8_buf_t, SIZE>>(uintBuffer<SIZE>(bigInt, false));
 }
 
-template <std::size_t SIZE>
-auto bigEndianBuffer(const BigInt& bigInt) -> std::array<boost::endian::big_uint8_buf_t, SIZE> {
+Y2KAOZNUMERIC_EXPORT template <std::size_t SIZE>
+inline auto bigEndianBuffer(const BigInt& bigInt) -> std::array<boost::endian::big_uint8_buf_t, SIZE> {
   return std::bit_cast<std::array<boost::endian::big_uint8_buf_t, SIZE>>(uintBuffer<SIZE>(bigInt, true));
 }
 
@@ -66,8 +65,8 @@ auto bigEndianBuffer(const BigInt& bigInt) -> std::array<boost::endian::big_uint
 // https://stackoverflow.com/questions/48233368/boostmultiprecisionpowm-differs-from-bigintegermodpow-in-java
 // There is a documented difference of how signed modulo is implemented with cpp_int.
 // As a simple fix, just add the divisor if the return value is negative:
-template <class T>
-auto powm(const T& a, const T& p, const T& c) -> T {
+Y2KAOZNUMERIC_EXPORT template <typename T>
+inline auto powm(const T& a, const T& p, const T& c) -> T {
   if constexpr (std::is_same_v<BigInt, boost::multiprecision::cpp_int>) {
     using std::abs; // summon the god of ADL
     T r = boost::multiprecision::powm(a, p, c);
